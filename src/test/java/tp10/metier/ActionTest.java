f36441131b6a8a578e856587fb13252bc1da7920
airol123 <63183652+airol123@users.noreply.github.com>
2021-03-08 11:36:38 +0100

    first pom commit

---
+++ src/test/java/tp10/metier/ActionTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp10.metier;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author carol
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+   
+}
